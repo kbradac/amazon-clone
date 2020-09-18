5ba8432d53d769f52f6b2cebd925a7bf86432933
kbradac <kristjan.bradac69@gmail.com>
2020-09-18 11:34:46 +0200

    second comm

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCrWX9FSMVZ0HPqQNWcJnYi-DFtDSetgcU",
+    authDomain: "clone-c6a0c.firebaseapp.com",
+    databaseURL: "https://clone-c6a0c.firebaseio.com",
+    projectId: "clone-c6a0c",
+    storageBucket: "clone-c6a0c.appspot.com",
+    messagingSenderId: "611130397820",
+    appId: "1:611130397820:web:d14e98d94d30b6227da5f9",
+    measurementId: "G-JLQFGECL55"
+  };
